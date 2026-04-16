31f40139841cf772af866b0b7c727115a74180ef
thaioneves <thais.oneves2101@gmail.com>
2026-04-16 20:09:19 -0300

    first commit

---
+++ estoque.js
@@ -0,0 +1,29 @@
+let produto;
+produto=prompt('Informe o nome do produto');
+
+let quantidade;
+quantidade=prompt('Informe a quantidade');
+
+if (quantidade<0 || produto=='') {
+    alert ('erro')
+} else {
+    if (quantidade<0) {
+        alert("produto esgotado");
+    } else {
+        if (quantidade<50) {
+            alert("estoque muito baixo");
+        } else {
+            if (quantidade<150) {
+                alert('estoque baixo');
+            } else {
+                if (quantidade<300) {
+                    alert('estoque médio');
+                } else {
+                   alert('estoque alto')
+                }
+            }
+        }
+    }
+   
+}
+
